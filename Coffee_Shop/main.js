1272bd9d30130ac4804dd76667e768af8fd56df1
Sinan Yozgatlı <75254489+sinanroot@users.noreply.github.com>
2022-03-06 23:03:12 +0300

    Add files via upload

---
+++ Coffee_Shop/main.js
@@ -0,0 +1,31 @@
+let search = document.querySelector('.search-box');
+
+document.querySelector('#search-icon').onclick = () => {
+    search.classList.toggle('active');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
